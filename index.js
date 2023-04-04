b14aa508ad364518ddafbba7fcf50266eee2a22f
Zehra <aydin.fzehra@gmail.com>
2023-04-04 09:47:15 +0300

    done

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(input) {
+    return input.charAt(0).toUpperCase() + input.slice(1);
+  }
+
+  static sanitize(input) {
+    return input.replace(/[^a-zA-Z0-9\s'-]/g, "");
+  }
+
+  static titleize(input) {
+    const exceptions = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
+    const words = input.split(" ");
+    for (let i = 0; i < words.length; i++) {
+      if (i === 0 || !exceptions.includes(words[i])) {
+        words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+      }
+    }
+    return words.join(" ");
+  }
 }
